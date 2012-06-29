49a769f90dfe085b681b789d6c140bfb3ebb2716
Johnny Freeman <prsjohnny@gmail.com>
2012-06-29 13:00:17 -0400

    split up the form building methods from the class loading methods of the Reform object

--- reform/FormBuilder.php
+++ reform/FormBuilder.php
@@ -23,6 +23,7 @@ use Reform\Element\Legend;
 use Reform\Field\Input\Input;
 use Reform\Field\Input\Checkbox;
 use Reform\Field\Input\Email;
+use Reform\Field\Input\Hidden;
 use Reform\Field\Input\Password;
 use Reform\Field\Input\Radio;
 use Reform\Field\Input\Submit;
@@ -31,17 +32,15 @@ use Reform\Field\Option;
 use Reform\Field\Textarea;
 use Reform\Field\Button;
 
-define('REFORM_PATH', realpath(__dir__) . DIRECTORY_SEPARATOR);
-
 /**
- * Reform
+ * Form Builder
  *
  * This is a static interface to make working with 
  * reform a little easier.
  */
-class Reform
+class FormBuilder
 {
-    private function __construct(){}
+    public function __construct(){}
     
     /**
      * Form
@@ -49,7 +48,7 @@ class Reform
      * @param   mixed   array of attributes or just the action (as a string)
      * @return  Reform\Element\Form    
      **/
-    public static function form($attributes='')
+    public static function new($attributes='')
     {
         return new Form($attributes);
     }
@@ -96,6 +95,17 @@ class Reform
     public static function password($name, $value = '', $attributes = array())
     {
         return new Password($name, $value, $attributes);
+    }
+    
+    /**
+     * Hidden
+     *
+     * @param   mixed   array of attributes or just the legend (as a string)
+     * @return  Reform\Field\Input\Hidden  
+     **/
+    public static function hidden($name, $value = '', $attributes = array())
+    {
+        return new Hidden($name, $value, $attributes);
     }
     
     /**
@@ -162,61 +172,5 @@ class Reform
     public static function label($text = '')
     {
         return new Label($text);
-    }
-
-    public static function registerAutoloader()
-    {
-        return spl_autoload_register('static::load', true, true);
-    }
-
-    // super quick autoloader for re:form
-    // it is NOT recommended for use with anything other 
-    // than loading reform objects
-    private static function load($classname)
-    {
-        $class_map = array(
-
-            // elements
-            'Reform\\Element\\Element'  => REFORM_PATH.'Element/Element.php',
-            'Reform\\Element\\Fieldset' => REFORM_PATH.'Element/Fieldset.php',
-            'Reform\\Element\\Form'     => REFORM_PATH.'Element/Form.php',
-            'Reform\\Element\\Label'    => REFORM_PATH.'Element/Label.php',
-            'Reform\\Element\\Legend'   => REFORM_PATH.'Element/Legend.php',
-
-            // fields
-            'Reform\\Field\\Field'              => REFORM_PATH.'Field/Field.php',
-            'Reform\\Field\\Button'             => REFORM_PATH.'Field/Button.php',
-            'Reform\\Field\\Input\\Input'       => REFORM_PATH.'Field/Input/Input.php',
-            'Reform\\Field\\Input\\Checkbox'    => REFORM_PATH.'Field/Input/Checkbox.php',
-            'Reform\\Field\\Input\\Email'       => REFORM_PATH.'Field/Input/Email.php',
-            'Reform\\Field\\Input\\Password'    => REFORM_PATH.'Field/Input/Password.php',
-            'Reform\\Field\\Input\\Radio'       => REFORM_PATH.'Field/Input/Radio.php',
-            'Reform\\Field\\Input\\Submit'      => REFORM_PATH.'Field/Input/Submit.php',
-            'Reform\\Field\\Select'             => REFORM_PATH.'Field/Select.php',
-            'Reform\\Field\\Option'             => REFORM_PATH.'Field/Option.php',
-            'Reform\\Field\\Textarea'           => REFORM_PATH.'Field/Textarea.php',
-
-            // validation rules
-            'Reform\\ValidationRule\\ValidationRule'    => REFORM_PATH.'ValidationRule/ValidationRule.php',
-            'Reform\\ValidationRule\\MatchesField'      => REFORM_PATH.'ValidationRule/MatchesField.php',
-            'Reform\\ValidationRule\\MatchesValue'      => REFORM_PATH.'ValidationRule/MatchesValue.php',
-            'Reform\\ValidationRule\\Required'          => REFORM_PATH.'ValidationRule/Required.php',
-
-            // exceptions
-            'Reform\\Exception\\ValidationFailedException' => REFORM_PATH.'Exception/ValidationFailedException.php',
-
-            // examples
-            'Reform\\Examples\\ContactForm\\ContactForm' => realpath(__dir__.'/..').'/examples/ContactForm/ContactForm.php',
-            'Reform\\Examples\\ContactForm\\ContactName' => realpath(__dir__.'/..').'/examples/ContactForm/ContactName.php',
-            'Reform\\Examples\\ContactForm\\ContactEmail' => realpath(__dir__.'/..').'/examples/ContactForm/ContactEmail.php',
-            'Reform\\Examples\\ContactForm\\ContactMessage' => realpath(__dir__.'/..').'/examples/ContactForm/ContactMessage.php',
-        );
-        
-        //echo '<pre>'; print_r($class_map[$classname]); echo '</pre>';
-
-        if (array_key_exists($classname, $class_map) && is_readable($class_map[$classname]))
-        {
-            require_once($class_map[$classname]);
-        }
     }
 }
