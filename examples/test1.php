801f65b8fe7e605f930f26c065093d42952fda04
Johnny Freeman <prsjohnny@gmail.com>
2014-07-10 12:15:55 -0400

    Cleaned up example for more clarity

--- examples/test1.php
+++ examples/test1.php
@@ -20,20 +20,22 @@ use Reform\ValidationRule\MatchesField;
  * EXAMPLE FORM
  */
 
-$form = Reform::form('');
+$form = Reform::form('')->append(array(
+	Reform::input('name')->setAttribute('id', 'name')->addRule(new Required),
+	Reform::email('email')->addRule(new Required),
+	$password1 = Reform::password('password1')->addRule(new Required),
+	Reform::password('password2')->addRule(new MatchesField($password1)),
+	Reform::select('account_type')->append(array(
+		Reform::option('foo'),
+		Reform::option('foo1'),
+		Reform::option('bar', 'foo'),
+		Reform::option('foo2'),
+	)),
+	Reform::submit('', 'Sign up')
+));
 
 //$form = Reform::form(array('id'=>'my_form'));
 
-Reform::input('name')->addRule(new Required)->appendTo($form)->setAttribute('id', 'name');
-Reform::email('email')->addRule(new Required)->appendTo($form);
-$password1 = Reform::password('password1')->addRule(new Required)->appendTo($form);
-Reform::password('password2')->addRule(new MatchesField($password1))->appendTo($form);
-$account_type = Reform::select('account_type')->appendTo($form);
-Reform::option('foo')->appendTo($account_type);
-Reform::option('foo1')->appendTo($account_type);
-Reform::option('bar', 'foo')->appendTo($account_type);
-Reform::option('foo2')->appendTo($account_type);
-Reform::submit('', 'Sign up')->appendTo($form);
 
 //echo '<pre>'; print_r($form); echo '</pre>'; die();
 
