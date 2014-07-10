091166035fdc133b6000ec7d65a2bb6ee3bab924
Johnny Freeman <prsjohnny@gmail.com>
2014-07-10 16:29:33 -0400

    added new matches validation to classloader

--- reform/ClassLoader.php
+++ reform/ClassLoader.php
@@ -58,11 +58,10 @@ abstract class ClassLoader
             'Reform\\Field\\Textarea'           => REFORM_PATH.'Field/Textarea.php',
 
             // validation rules
+            'Reform\\ValidationRule\\Matches'           => REFORM_PATH.'ValidationRule/Matches.php',
+            'Reform\\ValidationRule\\Required'          => REFORM_PATH.'ValidationRule/Required.php',
             'Reform\\ValidationRule\\ValidationRule'    => REFORM_PATH.'ValidationRule/ValidationRule.php',
             'Reform\\ValidationRule\\ValidEmail'        => REFORM_PATH.'ValidationRule/ValidEmail.php',
-            'Reform\\ValidationRule\\MatchesField'      => REFORM_PATH.'ValidationRule/MatchesField.php',
-            'Reform\\ValidationRule\\MatchesValue'      => REFORM_PATH.'ValidationRule/MatchesValue.php',
-            'Reform\\ValidationRule\\Required'          => REFORM_PATH.'ValidationRule/Required.php',
 
             // exceptions
             'Reform\\Exception\\ValidationFailedException' => REFORM_PATH.'Exception/ValidationFailedException.php',
